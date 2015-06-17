548ee393747942cb231d1acff744eca51e15e36b
marc-despland <marc.despland@art122-5.net>
2015-06-17 21:40:23 +0200

    first release of tcpserver

--- tcpserver/include/tcpserver.h
+++ tcpserver/include/tcpserver.h
@@ -1,6 +1,7 @@
 #ifndef _TCPSERVER_H
 #define _TCPSERVER_H
-typedef int (*connection_handler)(int);
+#include <sys/socket.h>
+typedef int (*connection_handler)(int, struct sockaddr_in , socklen_t);
 int listen_request(unsigned int port, unsigned int pool_size, connection_handler handler);
 
 #endif
--- tcpserver/src/tcpserver.c
+++ tcpserver/src/tcpserver.c
@@ -5,6 +5,9 @@
 #include <sys/wait.h>
 #include <strings.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "tcpserver.h"
 
 /*
 Action on SIGCHLD to manage Child termination
@@ -19,19 +22,18 @@ void received_sigchld_on_tcpserver(int sig) {
 void listen_sigchld() {
 	struct sigaction eventSigChld;
 	//Create the sigaction structure to handle SIGCHLD signal
-    sigemptyset(&eventSigChld.sa_mask);
-    eventSigChld.sa_flags=0;
-    eventSigChld.sa_handler= received_sigchld_on_tcpserver;
+	sigemptyset(&eventSigChld.sa_mask);
+	eventSigChld.sa_flags=0;
+	eventSigChld.sa_handler= received_sigchld_on_tcpserver;
 	sigaction(SIGCHLD,&eventSigChld, NULL);
 }
 
 int listen_request(unsigned int port, unsigned int pool_size, connection_handler handler) {
-	int mainfd,clientfd,n;
-	struct sockaddr_in server,client;
+	int mainfd,clientfd;
+	struct sockaddr_in server;
+	struct sockaddr_in client;
 	socklen_t clientsize;
 	pid_t     childpid,serverpid;
-	char buffer[100];
-	int go;
 
 	mainfd=socket(AF_INET,SOCK_STREAM,0);
 
@@ -43,32 +45,22 @@ int listen_request(unsigned int port, unsigned int pool_size, connection_handler
 
 	listen(mainfd,pool_size);
 	listen_sigchld();
-	go=1;
 	childpid=-1;
 	serverpid=getpid();
 	while (serverpid==getpid()) {
 		bzero(&client,sizeof(client));
 		clientsize=sizeof(client);
 		clientfd = accept(mainfd,(struct sockaddr *)&client,&clientsize);
-		printf("%d CHILDPID : %d clientfd=%d mainfd=%d errno=%d\n",getpid(),childpid, clientfd, mainfd, errno);
+		//printf("%d CHILDPID : %d clientfd=%d mainfd=%d errno=%d\n",getpid(),childpid, clientfd, mainfd, errno);
 		//When we receive SIGCHLD the access is unlock and return -1, ernno=4 (EINTR)
 		if (clientfd>0) {
 			if ((childpid = fork()) == 0) {
 				close (mainfd);
-				go=1;
-				while (go) {
-					n = recv(clientfd,buffer,sizeof(buffer),0);
-					printf("recv %d\n",n);
-					if (n>0) {
-						buffer[n]=0;
-						send(clientfd,buffer,n,0);
-					} else {
-						go=0;
-					}
-				}
+				handler(clientfd,client, clientsize);
 			}
-            printf("%d close clientfd %d\n", getpid(), clientfd);
-            close(clientfd);
+			//printf("%d close clientfd %d\n", getpid(), clientfd);
+			close(clientfd);
 		}
 	}
+	return 1;
 }
